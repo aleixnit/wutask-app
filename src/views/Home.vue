23518c2871e5241e2d1ef68c6010243b1a2fc1d1
Aleix <nitjac@gmail.com>
2023-02-10 11:29:50 +0100

    ttest precommit - vercel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@
         @updateTask="getTasks"
       />
     </div>
-    <Footer />
+    <!-- <Footer /> -->
   </div>
 </template>
 
@@ -28,7 +28,7 @@ import { useRouter } from "vue-router";
 import Nav from "../components/Nav.vue";
 import NewTask from "../components/NewTask.vue";
 import TaskItem from "../components/TaskItem.vue";
-import Footer from "../components/Footer.vue";
+// import Footer from "../components/Footer.vue";
 
 const taskStore = useTaskStore();
 
