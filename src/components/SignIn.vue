d11b8f39666ba288017c1a8c6116bb3ad6e98214
aleixnit <nitjac210@gmail.com>
2023-10-03 00:33:03 +0200

    signin page

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,52 +1,247 @@
 <!-- COMPONENTE BOILERPLATE -->
 
 <template>
-  <div class="fondo">
-    <div class="loginpage">
-      <img src="/assets/logowu3.png" alt="Logo Task App" />
-      <h3>¡Bienvenido al WuTask App!</h3>
-      <p class="refactClass">¡Mantén las tareas de tu dia a dia organizadas!</p>
+  <div id="body" class="hiddenloader">
 
-      <!-- inputs nuevos!!!!!!!!!!! -->
-      <form @submit.prevent="signIn">
-        <div class="inputSignin">
-          <input required="" type="email" v-model="email" class="input" />
-          <span class="highlight"></span>
-          <span class="bar"></span>
-          <label for="email">Email</label>
+    <div class="centrado" id="onload">
+    <div class="master-container">
+      <div class="container">
+        <div class="crescent">
+          <span></span><span></span>
         </div>
-
-        <div class="inputSignin">
-          <input required="" type="password" v-model="password" class="input" />
-          <span class="highlight"></span>
-          <span class="bar"></span>
-          <label for="password">Contraseña</label>
+        <div class="star s1">
+          <span></span><span></span>
         </div>
-        <div class="iniciarRegistro">
-          <!-- REGISTRARSE / SIGN UP -->
-          
-          <button type="submit" data-text="Awesome" class="button6">
-    <span class="actual-text">&nbsp;Iniciar&nbsp;</span>
-    <span class="hover-text" aria-hidden="true">&nbsp;Iniciar&nbsp;</span>
-</button>
-<!-- 
-          <button class="fancy" type="submit">
-            <span class="top-key"></span>
-            <span class="text">Iniciar Sesión</span>
-            <span class="bottom-key-1"></span>
-            <span class="bottom-key-2"></span>
-          </button> -->
-
-          <p>
-            No tienes una cuenta?
-            <PersonalRouter
-              :route="route"
-              :buttonText="buttonText"
-              class="sign-up-link"
+        <div class="star s2">
+          <span></span><span></span>
+        </div>
+        <div class="star s3">
+          <span></span><span></span>
+        </div>
+        <div class="star s4">
+          <span></span><span></span>
+        </div>
+        <div class="star s5">
+          <span></span><span></span>
+        </div>
+        <div class="small-star ss1">
+          <span></span><span></span>
+        </div>
+        <div class="small-star ss2">
+          <span></span><span></span>
+        </div>
+        <div class="small-star ss3">
+          <span></span><span></span>
+        </div>
+        <div class="small-star ss4">
+          <span></span><span></span>
+        </div>
+        <div class="small-star ss5">
+          <span></span><span></span>
+        </div>
+        <div class="small-star ss6">
+          <span></span><span></span>
+        </div>
+        <div class="small-star ss7">
+          <span></span><span></span>
+        </div>
+        <div class="small-star ss8">
+          <span></span><span></span>
+        </div>
+        <div class="hut">
+          <hr>
+          <hr>
+          <hr>
+          <hr>
+          <hr>
+          <hr>
+          <div class="doorway">
+            <hr class="split">
+          </div>
+        </div>
+        <div class="fire-pit">
+          <div class="fire">
+            <div class="inner-fire">
+            </div>
+          </div>
+          <hr class="log1">
+          <hr class="log2">
+        </div>
+        <div class="tree one">
+          <hr class="trunc">
+          <hr class="leaves">
+          <hr class="leaves">
+          <hr class="leaves">
+          <hr class="leaves">
+        </div>
+        <div class="tree two">
+          <hr class="trunc">
+          <hr class="leaves">
+          <hr class="leaves">
+          <hr class="leaves">
+          <hr class="leaves">
+        </div>
+        <div class="tree three">
+          <hr class="trunc">
+          <hr class="leaves">
+          <hr class="leaves">
+          <hr class="leaves">
+          <hr class="leaves">
+        </div>
+        <div class="tree four">
+          <hr class="trunc">
+          <hr class="leaves">
+          <hr class="leaves">
+          <hr class="leaves">
+          <hr class="leaves">
+        </div>
+        <div class="hill"></div>
+      </div>
+      <span style='font-family: "Montserrat"; font-size:1.3rem' class='loading-text'>Cargando...</span>
+    </div>
+  </div>
+  
+    <div class="signin_fondo">
+      <div class="signin_content">
+        <img src="/assets/wulogo.png" alt="Logo Task App" />
+        <h3>
+          ¡Bienvenido <br />al
+          <span style="color: rgb(255, 230, 0)">WuTask</span> App!
+        </h3>
+        <p>
+          Imagina una <span class="bold">aplicación de tareas</span> con un toque
+          épico inspirado en el legendario <span class="bold">clan Wu-Tang</span>.
+          Presentamos "TaskWu", una aplicación diseñada para que manejes tus
+          tareas diarias
+          <span class="bold">
+            con la misma pasión y determinación que los miembros del Wu-Tang Clan
+          </span>
+          ponen en su música.
+        </p>
+        <!-- inputs nuevos!!!!!!!!!!! -->
+        <form class="signin_form_box" @submit.prevent="signIn">
+          <!--INput new sept-->
+          <div class="signin_form">
+            <input
+              required=""
+              type="email"
+              v-model="email"
+              class="signin_input"
             />
-          </p>
+            <label for="email">
+              <span style="transition-delay: 0ms; left: 0px; top: -25px;">E</span>
+              <span style="transition-delay: 75ms; left: 20px; top: -25px;">m</span>
+              <span style="transition-delay: 150ms; left: 47px; top: -25px;">a</span>
+              <span style="transition-delay: 225ms; left: 64px; top: -25px;">i</span>
+              <span style="transition-delay: 300ms; left: 75px; top: -25px;">l</span>
+              <p
+                style="
+                  position: absolute;
+                  left: -8px;
+                  top: -15px;
+                  font-size: 17px;
+                  margin: 10px;
+                  color: gray;
+                  transition: 0.5s;
+                  pointer-events: none;
+                "
+              >
+                Email
+              </p>
+            </label>
+          </div>
+          <!--INput new sept-->
+          <div class="signin_form">
+            <input
+              required=""
+              type="password"
+              v-model="password"
+              class="signin_input"
+            />
+            <label for="password">
+              <span style="transition-delay: 0ms; left: 0px; top: -25px;">P</span>
+              <span style="transition-delay: 75ms; left: 20px; top: -25px;">a</span>
+              <span style="transition-delay: 150ms; left: 33px; top: -25px;">s</span>
+              <span style="transition-delay: 225ms; left: 47px; top: -25px;">s</span>
+              <span style="transition-delay: 300ms; left: 56px; top: -25px;">w</span>
+              <span style="transition-delay: 375ms; left: 72px; top: -25px;">o</span>
+              <span style="transition-delay: 450ms; left: 85px; top: -25px;">r</span>
+              <span style="transition-delay: 525ms; left: 95px; top: -25px;">d</span>
+              <p
+                style="
+                  position: absolute;
+                  left: -8px;
+                  top: -15px;
+                  font-size: 17px;
+                  margin: 10px;
+                  color: gray;
+                  transition: 0.5s;
+                  pointer-events: none;
+                "
+              >
+                Password
+              </p>
+            </label>
+          </div>
+          <div class="iniciarRegistro">
+            <!-- REGISTRARSE / SIGN UP -->
+            <button type="submit" data-text="Awesome" class="signin_button">
+              <span class="signin_button_span">INICIAR</span>
+              <span class="signin_button_second">
+                <svg
+                  width="50px"
+                  height="20px"
+                  viewBox="0 0 66 43"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                >
+                  <g
+                    id="arrow"
+                    stroke="none"
+                    stroke-width="1"
+                    fill="none"
+                    fill-rule="evenodd"
+                  >
+                    <path
+                      class="signin_button_one"
+                      d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z"
+                      fill="#FFFFFF"
+                    ></path>
+                    <path
+                      class="signin_button_two"
+                      d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z"
+                      fill="#FFFFFF"
+                    ></path>
+                    <path
+                      class="signin_button_three"
+                      d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z"
+                      fill="#FFFFFF"
+                    ></path>
+                  </g>
+                </svg>
+              </span>
+            </button>
+            <p>
+              No tienes una cuenta?
+              <PersonalRouter
+                :route="route"
+                :buttonText="buttonText"
+                class="signin_button_register"
+              />
+            </p>
+          </div>
+        </form>
+      </div>
+        <div class="signin_box2">
+          <div class="video-background">
+            <video autoplay muted loop class="video-bg">
+              <source src="../../assets/wutaskvideo2.mp4" type="video/mp4" />
+              Tu navegador no soporta el elemento de video.
+            </video>
+            <!-- Contenido de tu componente aquí -->
+              </div>
         </div>
-      </form>
     </div>
   </div>
   <!-- </div> -->
@@ -60,6 +255,13 @@ import { useRouter } from "vue-router";
 import { useUserStore } from "../stores/user";
 import { storeToRefs } from "pinia";
 
+ //PRELOADER PAGINA WEB
+ window.onload = function () {
+      setTimeout(function () {
+        $('#onload').fadeOut();
+        $('#body').removeClass('hiddenloader');
+      }, 1000); // 1500 milisegundos = 1.5 segundos
+    }
 // Route Variables
 const route = "/auth/signup";
 const buttonText = "¡Registrate!";
@@ -88,5 +290,4 @@ const signIn = async () => {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
